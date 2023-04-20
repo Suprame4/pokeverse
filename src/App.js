1c4d5f27b357a04768e0bdb8c63c5052b9194b30
Adrian G <adriancortesgonz@gmail.com>
2023-04-20 14:17:39 -0700

    initial commit part 1: fetch and search

--- src/App.js
+++ src/App.js
@@ -1,18 +1,65 @@
 import React from 'react';
+import {useState, useEffect} from 'react';
 import { Navigation } from './components/Navigation';
 import { PokemonCard } from './components/PokemonCard';
+import { InputGroup, Form } from 'react-bootstrap';
+import "./styles.css"
 
 const LIMIT = 150;
 const pokeApi = `https://pokeapi.co/api/v2/pokemon/?limit=${LIMIT}`;
 
 function App() {
+  const [pokeRaw, setPokeRaw] = useState([])
+  const [pokeFiltered, setPokeFiltered] = useState([])
 
+  const pokeRequest = async () => {
+    const res = await fetch(pokeApi)
+    const data = await res.json()
+
+    console.log("Results: ", data.results)
+
+    //set the data
+    setPokeRaw(data.results)
+    setPokeFiltered(data.results)
+  }
+
+  useEffect(() => {
+    pokeRequest()
+  }, [])
+
+  function handleChange(e){
+    //grab input value
+    const value = e.target.value
+
+    //regex to match input value
+    const regex = new RegExp(value, 'gi')
+    //filter matches from pokeRaw
+    const filtered = pokeRaw.filter(poke => {
+      return poke.name.match(regex) 
+    })
+    setPokeFiltered(filtered)
+  }
   return (
     <div data-testid="app">
       <Navigation />
+      <InputGroup>
+        <InputGroup.Text id="search-id">Search</InputGroup.Text>
+        <Form.Control 
+          placeholder="Name"
+          aria-label="Name"
+          aria-describedby="search-id"
+          onChange={handleChange}
+        />
+      </InputGroup>
 
       <h1>Pokemon should appear here</h1>
-      <PokemonCard />
+      <div class="container">
+        {pokeFiltered.map(poke => {
+          return (
+            <PokemonCard name={poke.name} url={poke.url} list={pokeFiltered}/>
+          )
+        })}
+      </div>
     </div>
   );
 }
--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -1,10 +1,49 @@
 import React from 'react';
+import {useState, useEffect} from 'react'
+import Card from "react-bootstrap/Card"
+import axios from "axios"
+
 
 function PokemonCard({ url, name }) {
 
+  const [pokeStats, setPokeStats] = useState(null)
+  
+  /*const pokeRequest2 = async () => {
+    const res = await fetch(url)
+    const data = await res.json()
+
+    console.log("Results 2: ", data)
+
+    setPokeStats(data)
+  }*/
+
+  useEffect(() => {
+    const pokeRequest2 = async () => {
+      const res = await axios.get(url)
+      setPokeStats(res.data)
+      console.log("RESULTS: ", res.data)
+    }
+    pokeRequest2()
+   
+  }, [])
+
   return (
     <div>
-        pokemon card
+        <Card>
+          {/* Double check on the item?.item?.item syntax */}
+          <Card.Img src={pokeStats?.sprites?.front_default}/>
+            <Card.Body>
+              <Card.Title>
+                {name}
+              </Card.Title> 
+              <Card.Text>
+                Abilities: 
+                  <ul>
+                    {pokeStats?.abilities.map(ability => <li>{ability.ability.name} </li>)}
+                  </ul>
+              </Card.Text>
+            </Card.Body>
+        </Card>
     </div>
   );
 }
