faaf2d4283a3a1acd386b5ce96017fef9fc470dc
Adrian G <adriancortesgonz@gmail.com>
2023-05-11 15:04:19 -0700

    complete pokeverse part 2

--- src/App.js
+++ src/App.js
@@ -1,75 +1,17 @@
 import React from 'react';
-import {useState, useEffect} from 'react';
-import { Navigation } from './components/Navigation';
-import { PokemonCard } from './components/PokemonCard';
-import { InputGroup, Form, Col, Container, Row} from 'react-bootstrap';
-import "./styles.css"
-
-const LIMIT = 150;
-const pokeApi = `https://pokeapi.co/api/v2/pokemon/?limit=${LIMIT}`;
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { Home } from './routes/Home'
+import { PokemonDetail} from './routes/PokemonDetail'
 
 function App() {
-  const [pokeRaw, setPokeRaw] = useState([])
-  const [pokeFiltered, setPokeFiltered] = useState([])
-
-  const pokeRequest = async () => {
-    const res = await fetch(pokeApi)
-    const data = await res.json()
-
-    console.log("Results: ", data.results)
-
-    //set the data
-    setPokeRaw(data.results)
-    setPokeFiltered(data.results)
-  }
-
-  useEffect(() => {
-    pokeRequest()
-  }, [])
-
-  function handleChange(e){
-    //grab input value
-    const value = e.target.value
-
-    //regex to match input value
-    const regex = new RegExp(value, 'gi')
-    //filter matches from pokeRaw
-    const filtered = pokeRaw.filter(poke => {
-      return poke.name.match(regex) 
-    })
-    setPokeFiltered(filtered)
-  }
+  
+  const router = createBrowserRouter([
+    { path: "/", element: <Home/>},
+    { path: "/:name", element: <PokemonDetail/> }
+  ])
+  
   return (
-    <div data-testid="app">
-      <Navigation />
-
-      <Container>
-        <Row className='mb-4'>
-          <Col sm='8' md='6' className='mx-auto'>
-            <InputGroup>
-              <InputGroup.Text id="search">Search</InputGroup.Text>
-              <Form.Control 
-              placeholder="Name"
-              aria-label="Name"
-              aria-describedby="search"
-              onChange={handleChange}
-          //alternative onChange
-          // onChange={(e) => setSearch(e.target.value)}
-              />
-            </InputGroup>
-          </Col>
-        </Row>
-
-
-      <div class="container">
-        {pokeFiltered.map(poke => {
-          return (
-            <PokemonCard name={poke.name} url={poke.url} list={pokeFiltered}/>
-          )
-        })}
-      </div>
-      </Container>
-    </div>
+    <RouterProvider router={router} />
   );
 }
 
---
+++ src/routes/PokemonDetail.js
@@ -0,0 +1,76 @@
+import React from 'react';
+import {useState, useEffect} from 'react';
+import {Typography, Card, Box} from '@mui/material';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import { useParams } from 'react-router-dom';
+import { Navigation } from '../components/Navigation';
+
+const PokemonDetail = () => {
+  const params = useParams();
+  const [pokeData, setPokeData] = useState();
+
+  useEffect(()=>{
+    const retrieve = async () => {
+      const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${params.name}`);
+      const data = await res.json();
+      setPokeData(data);
+    }
+    retrieve();
+  }, []);
+
+  if (pokeData === undefined) return null;
+
+  return <>
+      <Navigation/>
+      <div sx={{
+        display: "flex",
+        justifyContent: "center",
+        mx: "auto"
+      }}>
+      <Card sx={{
+        mx: 1,
+        width: 600,
+        height: 700,
+        }}> 
+        <CardMedia
+          image={pokeData.sprites.other["official-artwork"]["front_default"]}
+          title={pokeData.name}
+          sx={{ height: 400,
+                width: "60%",
+                objectFit: "cover",
+                mx: "auto"}}
+        />
+        <CardContent>
+          <Box sx={{
+            display: "flex",
+            justifyContent: "space-between"
+          }}>
+            <Typography gutterBottom variant="h5" component="div" className="poke-title-ext">
+              {pokeData.name}
+            </Typography>
+            <Typography gutterBottom variant="h6" color="text.secondary">
+              {pokeData.id}
+            </Typography>
+          </Box>
+          <Typography variant="body1" color="text.primary">height: {pokeData.height}</Typography>
+          <Typography variant="body1" color="text.primary">weight: {pokeData.weight}</Typography>
+          <Typography variant="body1" color="text.primary">
+            types:
+          </Typography>
+          {pokeData.types.map((t, idx) => <>
+          <Typography key={idx} variant="body2" color="text.secondary">{t.type.name}</Typography>
+          </>)}
+          <Typography variant="body1" color="text.primary">
+            abilities:
+          </Typography>
+          {pokeData.abilities.map((a, idx) => <>
+          <Typography key={idx} variant="body2" color="text.secondary">{a.ability.name}</Typography>
+          </>)}
+        </CardContent>
+      </Card>
+      </div>
+  </>
+}
+
+export { PokemonDetail };
