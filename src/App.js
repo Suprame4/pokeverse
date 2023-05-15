b658d4ef21b1bf8099165f8747561d5414666956
Adrian G <adriancortesgonz@gmail.com>
2023-05-14 19:55:34 -0700

    add context for state management

--- src/App.js
+++ src/App.js
@@ -2,16 +2,21 @@ import React from 'react';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import { Home } from './routes/Home'
 import { PokemonDetail} from './routes/PokemonDetail'
+import { FavoritesProvider} from './FavoritesProvider'
+import Favorites from './routes/Favorites'
 
 function App() {
   
   const router = createBrowserRouter([
     { path: "/", element: <Home/>},
-    { path: "/:name", element: <PokemonDetail/> }
+    { path: "/:name", element: <PokemonDetail/> },
+    { path: "/favorites", element: <Favorites/>}
   ])
   
   return (
-    <RouterProvider router={router} />
+    <FavoritesProvider>
+      <RouterProvider router={router} />
+    </FavoritesProvider>
   );
 }
 
